c7ee176cdd2fc381d2fb35a50d00670c5bca8533
USER <kodiugos@gmail.com>
2023-10-16 11:59:10 +0100

    File changes

--- myweb/settings.py
+++ myweb/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
-import environ
-
-env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
