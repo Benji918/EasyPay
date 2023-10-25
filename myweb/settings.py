3b2d0016a71044dad4c29de6075382300d9ee0e1
USER <kodiugos@gmail.com>
2023-10-25 18:10:37 +0100

    file changes

--- myweb/settings.py
+++ myweb/settings.py
@@ -129,7 +129,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
